7fd414be69cde8c0f1db5c6a00fc89126b3a71f6
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-09-29 08:54:52 +0530

    done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
+from sklearn.metrics import silhouette_score
 
 
 def get_sink_node_path():
@@ -9,11 +10,25 @@ def get_sink_node_path():
     x = [(y/4)**2 for y in y]
     return [x,y]
 
+def plot_silh(X):
+    sil=[]
+    for i in range(2,10):
+        KM = KMeans(n_clusters=i,max_iter=500)
+        KM.fit(X)
+        lables = KM.labels_
+        sil.append(silhouette_score(X,lables,metric='euclidean'))
+    plt.clf()
+    plt.plot(range(2,10),sil)
+    plt.draw()
+    print(sil.index(max(sil))+2)
+    print(sil)
+    return sil.index(max(sil))+2
 
-ncluster = int(input("Enter number of clusters : "))
-col = ['blue', 'green', 'c', 'm', 'y', 'k']
+
+col = ['blue', 'green', 'c', 'm', 'y', 'k', "violet", "indigo"]
 X = np.array([[28, 7], [36, 5], [32, 2], [56, 8], [47, 5], [75, 9], [34, 4], [56, 9], [28, 1], [33, 6]])
-kmeans = KMeans(n_clusters=ncluster, random_state=0).fit(X)
+ncluster=plot_silh(X)
+kmeans = KMeans(n_clusters=ncluster,max_iter=500).fit(X)
 y = kmeans.labels_
 centroids = kmeans.cluster_centers_
 print("Clusters :",y)
@@ -26,7 +41,7 @@ print("Centroids :", clusters_centroids)
 print("Redii's :", clusters_radii)
 fig, ax = plt.subplots(1,figsize=(7,5))
 for i in range(ncluster):
-    plt.scatter(X[y==i, 0], X[y==i, 1], s=100, c=col[i], label =f'Cluster {i}')
+    plt.scatter(X[y==i, 0], X[y==i, 1], s=100, c=col[i], label =f'Cluster {i + 1}')
     art = mpatches.Circle(clusters_centroids[i],clusters_radii[i], edgecolor=col[i],fill=False)
     ax.add_patch(art)
 plt.scatter(centroids[:,0],centroids[:,1],s=200, c='red', label = 'Centroids',marker = 'x')
