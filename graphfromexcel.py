b74a0e3f39c2b7f97d3392c74ede4ac262f870b0
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-11-08 19:39:45 +0530

    Grap prog

--- graphfromexcel.py
+++ graphfromexcel.py
@@ -1,10 +1,32 @@
 import pandas as pd
-dff = pd.ExcelFile('dara.xlsx')
-k = []
+import matplotlib.pyplot as plt
+import numpy as np
+dff = pd.ExcelFile('data.xlsx')
+d = {}
+for i in range(10):
+    d['Node'+str(i+1)] = []
 for sheet in dff.sheet_names:
     df = pd.read_excel('data.xlsx',sheet)
-    l = []
     for i in range(10):
-        l.append(df['Node'+str(i+1)])
-    k.append(l)
+        d['Node'+str(i+1)].append(df['Node'+str(i+1)])
+# print(d['Node1'][0].tolist())
+# print(d['Node1'][0][1])
 dff.close()
+
+col = ['b*--', 'r--', 'm', 'y', 'k', "violet", "indigo"]
+name = ["Static", "Mobility"]
+i = 0
+figure, axes = plt.subplots(nrows=2, ncols=5)
+for rows in axes:
+    for ax1 in rows:
+        for j in range(len(d['Node'+str(i+1)])):
+            L = d['Node'+str(i+1)][j].tolist()
+            l = []
+            for I in range(0,len(L),200):
+                l.append(L[I])
+            ax1.plot(l, range(len(l)), col[j],label = name[j%2])
+            ax1.set_title('Node'+str(i+1))
+            ax1.legend()
+            
+        i+=1
+plt.show()
